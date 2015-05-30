bed02867f2db5392b6659216dca1195f9201b4f4
roll <roll@respect31.com>
2015-05-30 19:38:59 +0300

    major improvements

--- tasks/pages.js
+++ tasks/pages.js
@@ -3,18 +3,24 @@ var gulp = require('gulp');
 var data = require('../loaders/data');
 var packages = require('../loaders/packages');
 
+// Config
+packages.swig.setDefaults({'cache': false});
 
 // Build
 gulp.task('pages:build', function() {
     return gulp.src('pages/**/*')
         .pipe(packages.frontmatter()).on("data", function(file) {
             packages.assign(file, file.frontMatter);
-            file.template = file.template || file.layout || 'base.html';
+            file.template = file.template || file.layout;
+            file.scope = file.scope;
             delete file.frontMatter;
         })
         .pipe(packages.gulpsmith()
             .metadata(data)
-            .use(packages.markdown())
+            .use(packages.branch()
+                .pattern('.md')
+                .use(packages.markdown())
+            )
             .use(packages.excerpts())
             .use(packages.permalinks({
                 pattern: ':permalink',
@@ -22,6 +28,13 @@ gulp.task('pages:build', function() {
             }))
             .use(packages.templates({
                 engine: 'swig',
+                inPlace: true,
+                autoescape: false,
+                moment: packages.moment,
+            }))
+            .use(packages.templates({
+                engine: 'swig',
+                inPlace: false,
                 autoescape: false,
                 directory: 'layouts',
                 moment: packages.moment,
