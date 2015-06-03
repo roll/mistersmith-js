aa14b902c60f7eefdf263ec5c02e7cb96543bd20
roll <roll@respect31.com>
2015-06-03 12:15:48 +0300

    improved scripts task

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -6,7 +6,11 @@ var stack = require('./loaders/stack')();
 // Build
 gulp.task('scripts:build', function() {
     return gulp.src('scripts/**/*.coffee')
+        .pipe(stack.sourcemaps.init())
         .pipe(stack.coffee({bare: true}).on('error', stack.util.log))
+        .pipe(stack.concat('main.js'))
+        .pipe(stack.uglify())
+        .pipe(stack.sourcemaps.write('maps'))
         .pipe(gulp.dest('build/scripts'));
 });
 
