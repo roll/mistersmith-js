c6dfb64cecee60e2670e3a551fb1d5929db0a27f
roll <roll@respect31.com>
2015-06-03 12:56:21 +0300

    added imagemin

--- tasks/images.js
+++ tasks/images.js
@@ -1,10 +1,15 @@
 'use strict';
 var gulp = require('gulp');
+var stack = require('./loaders/stack')();
 
 
 // Build
 gulp.task('images:build', function() {
     return gulp.src('images/**/*')
+        .pipe(stack.cache(stack.imagemin({
+              progressive: true,
+              interlaced: true,
+        })))
         .pipe(gulp.dest('build/images'));
 });
 
