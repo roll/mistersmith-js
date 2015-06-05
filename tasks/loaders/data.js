9c555ab24198706366b2c989e46aee3d48d93457
roll <roll@respect31.com>
2015-06-05 12:07:27 +0300

    implemented naive recursive data loader close #16

--- tasks/loaders/data.js
+++ tasks/loaders/data.js
@@ -1,13 +1,27 @@
 'use strict';
 var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
 var yaml = require('js-yaml');
 
 
 module.exports = function(options) {
-    var data = {
-        'site': yaml.load(fs.readFileSync('data/site.yml')),
-        'stack': yaml.load(fs.readFileSync('data/stack.yml')),
-    };
+    var data = {};
+    var files = glob.sync('data/**/*.*');
+    files.forEach(function(file) {
+        var rpath = path.relative('data', file);
+        var parts = rpath.split(path.sep);
+        var scope = data;
+        parts.forEach(function(part, index) {
+            if (index < parts.length - 1) {
+                scope[part] = scope.part || {};
+                scope = scope[part];
+            } else {
+                var key = path.basename(part, '.yml');
+                scope[key] = yaml.load(fs.readFileSync(file));
+            }
+        });
+    });
     if (options && options.key) {
         return data[options.key];
     }
