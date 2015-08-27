b00c9321738ab40d86ff4b3195ea0ea3f9900082
roll <roll@respect31.com>
2015-08-27 11:00:34 +0300

    added babel support; close #56

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -11,6 +11,7 @@ gulp.task('scripts:build', function() {
     return gulp.src('scripts/**/*.js')
         .pipe(stack.if(watch, stack.plumber(gulp.meta.handlers.error)))
         .pipe(stack.sourcemaps.init())
+        .pipe(stack.babel())
         .pipe(stack.concat('main.js'))
         .pipe(stack.uglify())
         .pipe(stack.sourcemaps.write('maps'))
