e9eade4c20e74f5f9d41057a24127401523974d3
roll <roll@respect31.com>
2015-08-28 20:35:21 +0300

    Update 2015-08-28T17:35:17.913Z

---
+++ develop/scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+jQuery(function () {
+  anchors.options = {
+    placement: 'right',
+    visible: 'always',
+    icon: '¶'
+  };
+  anchors.add('h2, h3');
+});
+"use strict";
+//# sourceMappingURL=maps/main.js.map
---
+++ product/scripts/main.js
@@ -0,0 +1,2 @@
+"use strict";jQuery(function(){anchors.options={placement:"right",visible:"always",icon:"¶"},anchors.add("h2, h3")});
+//# sourceMappingURL=maps/main.js.map
